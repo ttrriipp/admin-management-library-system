77f0afab6999ec767883b8888286ae96a31aebb3
freezy29 <189191275+freezy29@users.noreply.github.com>
2025-04-22 17:21:12 +0800

    try push

--- AdminManagementLibrarySystem/Program.cs
+++ AdminManagementLibrarySystem/Program.cs
@@ -17,7 +17,7 @@ namespace AdminManagementLibrarySystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
+            //Try commit
         }
     }
 }
