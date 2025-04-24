bdcb95de2cb9358fc0e487ee562f8e7bfda0247a
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-04-24 11:18:23 +0800

    Create book model

---
+++ AdminManagementLibrarySystem/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AdminManagementLibrarySystem.Models
+{
+    internal class Book
+    {
+        private static string uuid = Guid.NewGuid().ToString();
+        public string BookID { get; } = uuid;
+        public string Title { get; set; } 
+        public string Author { get; set; } 
+        public string Publisher { get; set; } 
+        public string YearPublished { get; set; }
+        public string ISBN { get; set; } = "N/A"; 
+        public string Category { get; set; }
+        public bool AvailabilityStatus { get; set; } = true;
+
+				
+    }
+}
