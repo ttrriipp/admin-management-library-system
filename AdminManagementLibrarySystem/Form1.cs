a2f0dd591243161a4ea0ff9be2ad897e2abe155d
freezy29 <189191275+freezy29@users.noreply.github.com>
2025-04-30 22:21:38 +0800

    database connection admin

--- AdminManagementLibrarySystem/Form1.cs
+++ AdminManagementLibrarySystem/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using AdminManagementLibrarySystem.Models;
 
 namespace AdminManagementLibrarySystem
 {
     public partial class Form1 : Form
-    {
+    {   
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Admin admin = new Admin("asdad", "asjkfdhnas");
         }
     }
 }
--- AdminManagementLibrarySystem/Models/Admin.cs
+++ AdminManagementLibrarySystem/Models/Admin.cs
@@ -1,4 +1,8 @@
-﻿namespace AdminManagementLibrarySystem.Models
+﻿using MySql.Data.MySqlClient;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace AdminManagementLibrarySystem.Models
 {
     internal class Admin : User
     {
@@ -8,6 +12,16 @@
             this.FirstName = firstName;
             this.LastName = lastName;
             this.FullName = $"{FirstName} {LastName}";
+        }
+        public void adminDb_add()
+        {
+            string connect = "server=localhost;user id=root;password=;database=librarysys";
+            string queries = "INSERT INTO `admin_acc_db` (`adminid`, `username_admin`, `password_admin`) VALUES (NULL, 'admin1', 'admin1')";
+            MySqlConnection newconn = new MySqlConnection(connect);
+            MySqlCommand comm = new MySqlCommand(queries, newconn);
+            MySqlDataReader read;
+            newconn.Open();
+            read = comm.ExecuteReader();
         }
     }
 }
